1e1e778ef624598764a8674b5ce6573363c5e81f
Felipe <plangloi@student.42.fr>
2024-11-06 15:34:50 +0100

    finito

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -5,60 +5,89 @@
 
 #include "RPN.hpp"
 
-		std::stack<int> _array;
-		std::stack<int> _arrayTmp;
-		
-		ReversePolish::ReversePolish(): _array(), _arrayTmp(), _index(0){}
-		
-		// ReversePolish::ReversePolish(const ReversePolish &src){
-		// 	if(this != &src){
-		// 		for(int i = 0; src._array; ++i){
-		// 			this->_array = src._array;
-		// 		}
-		// 		for(int i = 0; src._arrayTmp; ++i){
-		// 			this->_array = src._arrayTmp;
-		// 		}	
-		// 	}
-		// }
-
-		// ReversePolish &ReversePolish::operator =(const ReversePolish &cpy){
-		// 	if(this != &cpy){
-		// 		for(int i = 0; cpy._array[i]; ++i){
-		// 			this->_array[i] = cpy._array[i];
-		// 		}
-		// 		for(int i = 0; cpy._arrayTmp[i]; ++i){
-		// 			this->_array[i] = cpy._arrayTmp[i];
-		// 		}	
-		// 	}
-		// 	return *this;
-		// }
-
-		ReversePolish::~ReversePolish(){}
-
-		bool ReversePolish::storeNumber(char *av){
-			
-    	std::string input = static_cast<std::string>(av);
-		for (size_t i = 0; i < input.length(); ++i) {
-        char c = input[i];
-        if ((c >= '0' && c <= '9') || c == '+' || c == '-' || c == '*' || c == '/') {
-            _array.push(std::string(1, c));
-        } else {
-            std::cerr << "Invalid character found: " << c << std::endl;
-        }
-    }
-
-		}
-
-		std::stack<std::string> ReversePolish::splitRes(){
-			
-			std::stack
-			while(_array.size() > 3){
-				_arra
-			}
-
-		}
-
-		std::stack<std::string> ReversePolish::solveCalcul(){
-			
+	std::stack<int> _array;
+	std::stack<int> _arrayTmp;
 	
+	// ReversePolish::ReversePolish(): _array(), _arrayTmp(), _index(0){}
+	
+	// ReversePolish::ReversePolish(const ReversePolish &src){
+	// 	if(this != &src){
+	// 		for(int i = 0; src._array; ++i){
+	// 			this->_array = src._array;
+	// 		}
+	// 		for(int i = 0; src._arrayTmp; ++i){
+	// 			this->_array = src._arrayTmp;
+	// 		}	
+	// 	}
+	// }
+	// ReversePolish &ReversePolish::operator =(const ReversePolish &cpy){
+	// 	if(this != &cpy){
+	// 		for(int i = 0; cpy._array[i]; ++i){
+	// 			this->_array[i] = cpy._array[i];
+	// 		}
+	// 		for(int i = 0; cpy._arrayTmp[i]; ++i){
+	// 			this->_array[i] = cpy._arrayTmp[i];
+	// 		}	
+	// 	}
+	// 	return *this;
+	// }
+	ReversePolish::~ReversePolish(){}
+	int makeCalcul(int nb1, int nb2, char who){
+		
+		int result = 0;
+		
+		switch(who){
+			case '+':
+				result = nb2 + nb1; 
+			case  '-':
+				result = nb2 - nb1;
+			case '*' :
+				result = nb2 * nb1;
+			case '/' :
+				{if (nb1 == 0|| nb2 == 0){
+					std::cerr << "Division par 0 impossible \n";
+					throw();}
+				else
+					result = nb2 / nb1;
+			}
+			default :
+				std::cerr << "Wrong type\n";
 		}
+		return result;
+	}
+
+	bool ReversePolish::checkInput(char c, int who){
+		if(c >= '0' && c <= '9' && who == 1)
+			return true ;
+		if(strchr("+-*/", c) && who == 0)
+			return true;
+	return false;
+	}
+
+	void ReversePolish::calculator(char *av){
+		std::stringstream ss(av);
+		std::string input;
+		int signNb = 0;
+		int number = 0;
+		char sign;
+
+		while (ss >> input){
+			int i = 0;
+		while(ss >> input){
+			if(checkInput(input[i], 1)){
+				_array.push(input);
+				number++;
+				}
+			if(checkInput(input[i], 0)){
+				signNb++;
+				sign = input[i];
+				break ;}
+			i++;
+		}
+		int nb1 = atoi(input.c_str());
+		_array.pop();
+		int nb2 = atoi(input.c_str());
+		_array.pop();
+		makeCalcul(nb1,nb2,sign);
+	}
+	}
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -11,14 +11,12 @@
 class ReversePolish{
 	private :
 		std::stack<std::string> _array;
-		std::stack<std::string> _arrayTmp;
-		int _index;
 	public :
 		ReversePolish();
 		ReversePolish(const ReversePolish & src);
 		ReversePolish & operator=(const ReversePolish &cpy);
 		~ReversePolish();
-		bool storeNumber(char *input);
-		std::stack<std::string> splitRes();
-		std::stack<std::string> solveCalcul();
+		int makeCalcul(int nb1, int nb2, char who);
+		bool checkInput(char c, int who);
+		void calculator(char *input);
 };
